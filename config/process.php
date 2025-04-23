4d318da090cd0f50a56de75194643392fc48ddb5
jeann <jeannguilherme30@gmail.com>
2025-04-23 00:20:31 -0300

    feat(remover-contato) - remove contato ao clickar no icone de exclusao

--- config/process.php
+++ config/process.php
@@ -53,6 +53,22 @@
                     $error = $e->getMessage();
                     echo "Erro: $error";
                 }
+        } else if($data["type"] === "delete"){
+            $id = $data["id"];
+
+            $query = "DELETE FROM contacts WHERE id = :id";
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":id", $id);
+
+            try{    
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato removido com sucesso";
+            } catch(PDOExpection $e){
+                    $error = $e->getMessage();
+                    echo "Erro: $error";
+            }
         }
 
         //redirect home
